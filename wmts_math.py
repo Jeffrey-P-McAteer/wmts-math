19787a30a34eb7952fcf62cba263e083746e81b8
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-06-29 10:42:49 -0400

    stuff

--- wmts_math.py
+++ wmts_math.py
@@ -4,6 +4,8 @@ import sys
 import urllib
 import urllib.request
 import pip
+import json
+import io
 
 pkgs = os.path.join(os.path.dirname(__file__), 'our-site-packages')
 sys.path.append(pkgs)
@@ -16,6 +18,16 @@ except:
   ])
   import xmltodict
 
+
+try:
+  import PIL
+except:
+  pip.main([
+    'install', f'--target={pkgs}', 'Pillow'
+  ])
+  import PIL
+import PIL.Image
+
 def get(url):
   with urllib.request.urlopen(url) as response:
    return response.read()
@@ -23,6 +35,14 @@ def get(url):
 def get_xml_as_dict(url):
   with urllib.request.urlopen(url) as response:
    return xmltodict.parse(response.read().decode('utf-8'))
+
+def get_capabilities_dict(url):
+  return get_xml_as_dict(f'{url}service=WMTS&request=GetCapabilities')
+
+def get_tile_png_rowcol(url, layer, matrix_num, row, col):
+  png_bytes = get(f'{url}SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER={layer}&STYLE=default&FORMAT=image/png&TILEMATRIX={matrix_num}&TILEROW={row}&TILECOL={col}')
+  return PIL.Image.open(io.BytesIO(png_bytes))
+
 
 
 
@@ -36,8 +56,10 @@ def main(args=sys.argv):
 
   print(f'Requesting a chip containing {lon_x},{lat_y} from {wmts_url}')
 
-  capabilities = get_xml_as_dict(f'{wmts_url}service=WMTS&request=GetCapabilities')
-  print(f'capabilities = {capabilities}')
+  capabilities = get_capabilities_dict(wmts_url)
+  print(f'capabilities = {json.dumps(capabilities, indent=2)}')
+
+
 
 
 
